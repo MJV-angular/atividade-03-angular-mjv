e3133ee099efd200ea956a944e63b5ca2926aea2
NaianeReis27 <email.annereis@gmail.com>
2023-03-19 00:36:41 -0300

    feat: atividade completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'atividade-02-angular';
+  title = 'atividade-03-angular';
 }
